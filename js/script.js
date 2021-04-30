44be12b8d219b7fafd16b33f96b01a3507d4cd1f
Henrique Black <67487181+hnrqblck@users.noreply.github.com>
2021-04-30 20:07:27 -0300

    Convertion Functionality

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const BINARY_USER_INPUT = document.querySelector('#userInput');
-let updatedValue;
+const CONVERTED_TO_DEC = document.querySelector('.convertedToDec');
+const CONVERT_BUTTON = document.querySelector('#convert-btn');
+let reversedBinaryValue = [];
+
 BINARY_USER_INPUT.addEventListener('keydown', checkBinaryInput);
 BINARY_USER_INPUT.addEventListener('input', updateBinaryValue);
+CONVERT_BUTTON.addEventListener('click', convertBinaryToDecimals);
 
 function checkBinaryInput(event) {
     const NOT_ZERO_OR_ONE = event.key !== "0" && event.key !== "1" && event.key !== "Backspace";
-    
+
     console.log(event.key);
     if (NOT_ZERO_OR_ONE) {
         alert('Type a valid binary value! 0 or 1');
@@ -14,9 +18,23 @@ function checkBinaryInput(event) {
 
 function updateBinaryValue(event) {
     BINARY_USER_INPUT.textContent = event.target.value;
-    updatedValue = BINARY_USER_INPUT.textContent;
-    const MAX_INPUT_LENGTH = BINARY_USER_INPUT.textContent.length > 8;
-    console.log(updatedValue);
+    const value = BINARY_USER_INPUT.textContent;
+    // STUDY MAP TO UNDERSTAND WHY DOES THIS WORK:
+    const updatedValue = value.split``.map(x=>+x); 
+    reversedBinaryValue = updatedValue.reverse();
 }
 
+function getLengthOfValue(number, index) {
+    return number === 1 ? (1 * 2 ** index) : 0;
+}
 
+function convertBinaryToDecimals(event) {
+    event.preventDefault();
+    let sum = 0; 
+    for (const [index, bi] of reversedBinaryValue.entries()) {
+        let result = getLengthOfValue(bi, index);
+        sum += result;
+    }
+    console.log(sum);
+    CONVERTED_TO_DEC.innerHTML += `<p><strong>${reversedBinaryValue.reverse``.join("")}</strong>: ${sum}</p>`;
+}
